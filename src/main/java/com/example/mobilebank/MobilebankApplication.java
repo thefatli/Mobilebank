fa9f305c96f49fe3f753ef329352a8984e5bf514
thefatli <3105340836@qq.com>
2024-07-01 17:09:43 +0800

    首次提交

---
+++ src/main/java/com/example/mobilebank/MobilebankApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobilebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mobilebank/MobilebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobilebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
